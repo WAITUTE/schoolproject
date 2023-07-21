7b1713c3eb070f731c29aececbef4e4a72835c37
WAITUTE <109899422+WAITUTE@users.noreply.github.com>
2023-07-21 22:49:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace calender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.noteTextBox = new System.Windows.Forms.TextBox();
+            this.monthCalendar = new System.Windows.Forms.MonthCalendar();
+            this.calenderListView = new System.Windows.Forms.ListView();
+            this.dateColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.noteColumnHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.removeButton = new System.Windows.Forms.Button();
+            this.addButton = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIconContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(-1, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.noteTextBox);
+            this.splitContainer1.Panel1.Controls.Add(this.monthCalendar);
+            this.splitContainer1.Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer1_Panel1_Paint);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.calenderListView);
+            this.splitContainer1.Panel2.Controls.Add(this.removeButton);
+            this.splitContainer1.Panel2.Controls.Add(this.addButton);
+            this.splitContainer1.Size = new System.Drawing.Size(786, 461);
+            this.splitContainer1.SplitterDistance = 249;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // noteTextBox
+            // 
+            this.noteTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.noteTextBox.Location = new System.Drawing.Point(470, 3);
+            this.noteTextBox.Multiline = true;
+            this.noteTextBox.Name = "noteTextBox";
+            this.noteTextBox.Size = new System.Drawing.Size(313, 243);
+            this.noteTextBox.TabIndex = 1;
+            // 
+            // monthCalendar
+            // 
+            this.monthCalendar.CalendarDimensions = new System.Drawing.Size(2, 1);
+            this.monthCalendar.Location = new System.Drawing.Point(0, 0);
+            this.monthCalendar.MaxSelectionCount = 1;
+            this.monthCalendar.Name = "monthCalendar";
+            this.monthCalendar.TabIndex = 0;
+            // 
+            // calenderListView
+            // 
+            this.calenderListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.calenderListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.dateColumnHeader,
+            this.noteColumnHeader});
+            this.calenderListView.FullRowSelect = true;
+            this.calenderListView.HideSelection = false;
+            this.calenderListView.Location = new System.Drawing.Point(13, 43);
+            this.calenderListView.Name = "calenderListView";
+            this.calenderListView.Size = new System.Drawing.Size(760, 153);
+            this.calenderListView.TabIndex = 2;
+            this.calenderListView.UseCompatibleStateImageBehavior = false;
+            this.calenderListView.View = System.Windows.Forms.View.Details;
+            this.calenderListView.SelectedIndexChanged += new System.EventHandler(this.calenderListView_SelectedIndexChanged);
+            // 
+            // dateColumnHeader
+            // 
+            this.dateColumnHeader.Text = "Date";
+            this.dateColumnHeader.Width = 120;
+            // 
+            // noteColumnHeader
+            // 
+            this.noteColumnHeader.Text = "Note";
+            this.noteColumnHeader.Width = 200;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Enabled = false;
+            this.removeButton.Location = new System.Drawing.Point(103, 14);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.TabIndex = 1;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(13, 14);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.notifyIconContextMenu;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Windows Form Calender";
+            this.notifyIcon.Visible = true;
+            // 
+            // notifyIconContextMenu
+            // 
+            this.notifyIconContextMenu.Name = "notifyIconContextMenu";
+            this.notifyIconContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox noteTextBox;
+        private System.Windows.Forms.MonthCalendar monthCalendar;
+        private System.Windows.Forms.ListView calenderListView;
+        private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ColumnHeader dateColumnHeader;
+        private System.Windows.Forms.ColumnHeader noteColumnHeader;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyIconContextMenu;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string date = monthCalendar.SelectionStart.ToShortDateString();
+            string note = noteTextBox.Text;
+            ListViewItem newItem = new ListViewItem(new[] { date, note });
+            calenderListView.Items.Add(newItem);
+            noteTextBox.Text = "";
+            notifyIcon.ShowBalloonTip(1000, "item added", "Added new event", ToolTipIcon.Info);
+
+        }
+
+        private void calenderListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            removeButton.Enabled = calenderListView.SelectedItems.Count > 0;
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            foreach(ListViewItem item in calenderListView.Items) {  
+                calenderListView.Items.Remove(item);
+             }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIconContextMenu.Items.Add("Exit", null, ExitContextMenu_Click);
+        }
+
+        private void ExitContextMenu_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
